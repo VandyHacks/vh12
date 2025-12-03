b3c07092e229dd8de0010840ba726bbe93a294ce
noahl <noahl25@lakesideschool.org>
2025-12-02 18:29:06 -0600

    fix

--- app/api/health.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Response = {
-    message: string
-}
-
-export default function handler(res: NextApiResponse<Response>) {
-    res.status(200).json({ message: "success" })
-}
